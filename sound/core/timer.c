cfdc9687f955a89cb56d84a6cad46f5e39f3d46f
Takashi Iwai <tiwai@suse.de>
2016-02-08 17:36:25 +0100

    UPSTREAM: ALSA: timer: Fix again the wrong instance passed to slave callbacks
    Change-Id: I7a37e93d4804e08f2f51ff09f4a39d9ab75e5760

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -392,6 +392,7 @@ unsigned long snd_timer_resolution(struct snd_timer_instance *timeri)
 static void snd_timer_notify1(struct snd_timer_instance *ti, int event)
 {
 	struct snd_timer *timer;
+	unsigned long flags;
 	unsigned long resolution = 0;
 	struct snd_timer_instance *ts;
 	struct timespec tstamp;
@@ -415,9 +416,11 @@ static void snd_timer_notify1(struct snd_timer_instance *ti, int event)
 		return;
 	if (timer->hw.flags & SNDRV_TIMER_HW_SLAVE)
 		return;
+	spin_lock_irqsave(&timer->lock, flags);
 	list_for_each_entry(ts, &ti->slave_active_head, active_list)
 		if (ts->ccallback)
 			ts->ccallback(ts, event + 100, &tstamp, resolution);
+	spin_unlock_irqrestore(&timer->lock, flags);
 }
 
 /* start/continue a master timer */
